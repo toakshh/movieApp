53425ea8233f3614af73575f4e81189694541cef
Akshat <akshnnd@gmail.com>
2025-05-29 22:13:13 +0530

    [[UX improve]] Added no result found on search page

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -74,6 +74,14 @@ const Search = () => {
               )}
           </>
         }
+        ListEmptyComponent={
+          !moviesLoading && !moviesError ?
+              <View className='mt-10 px-5'>
+                <Text className='text-center text-gray-500'>
+                  {searchQuery.trim() ? 'No results found' : 'Search for a movie'}
+                </Text>
+              </View> : null
+        }
         scrollEnabled={true}
       />
     </View>
